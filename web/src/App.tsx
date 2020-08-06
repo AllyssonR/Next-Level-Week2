840f01cf1110553efbaf318dfaa95fdd989815c0
AllyssonR <AllyssonR@users.noreply.github.com>
2020-08-05 22:12:17 -0300

    Ending the first day

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './assets/styles/global.css';
-import Landing from './pages/Landing';
+import Router from './router';
 
 function App() {
   return (
-   <Landing/>
+   <Router/>
   );
 }
 
